52a42ae8e70881f070b4b226cb7270d6f2012900
Sahil <102948492+Justsah1l@users.noreply.github.com>
2022-06-01 17:50:38 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,8 +17,14 @@ client.on('qr', qr => {
 });
 
 client.on('message', async message => {
-  const command = message.body.slice(1).trim().split(/ +/).shift().toLowerCase()
-  
+  //const command = message.body.slice(1).trim().split(/ +/).shift().toLowerCase()
+  //const args = message.body.slice(7).trim().split(/ +/).shift().toLowerCase()
+  //const args = message.body.slice(1).split(/ +/);
+  //const command = args.shift().toLowerCase();
+  const prefix = '!'
+  const args = message.body.slice(prefix.length).split(/ +/)
+  const command = args.shift().toLowerCase()
+
     switch(command)
     {
       case'ping':
@@ -30,15 +36,21 @@ client.on('message', async message => {
       case 'meme':
         const meme = await axios("https://meme-api.herokuapp.com/gimme/memes")
         .then(res => res.data)
-
-        //const media = MessageMedia.fromUrl(meme.url);
         client.sendMessage(message.from, await MessageMedia.fromUrl(meme.url))
         break
       case 'covid':
-        const info = await axios("https://coronavirus-19-api.herokuapp.com/countries/india")
+        //if (!args[0]) return message.reply('you need to enter a country name')
+        //const url = `https://coronavirus-19-api.herokuapp.com/countries/${args}`
+        //const info = await axios(url)
+        //.then(res => res.data)
+        //message.reply(`INDIA has a total of : + \*\*${info.cases}\*\*\ + covid cases ` ,)
+        if (!args) return message.reply('you need to enter a country name')
+        const url = `https://coronavirus-19-api.herokuapp.com/countries/${args}`
+        const info  = await axios(url)
         .then(res => res.data)
-        message.reply(`INDIA has a total of : + \*\*${info.cases}\*\*\ + covid cases ` ,)
-        break
+        let text = info.country.toUpperCase()
+        message.reply(`${text} has a total of ${info.cases} cases right now \n${text} has a total of ${info.deaths} deaths right now` ,)
+        
 
     }
 	
@@ -51,4 +63,10 @@ client.on('ready', () => {
 });
 
 client.initialize();
+
+
+
+
+
+
 
